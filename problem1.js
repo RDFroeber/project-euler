7f5660bc2b7aaecd7a45c2eaa33600e083d8cbab
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-18 11:46:50 -0500

    Commenting added to problem and well as JS->jQuery revisions

--- problem1.js
+++ problem1.js
@@ -3,31 +3,34 @@ $(function(){
 
    button.click(function(e){
       e.preventDefault();
-      var max = document.getElementById("max").value
+      // Grab the input for max number in range
+      var max = $("#max").val();
 
       var nums = []
+      // Create an array with all numbers up to the max
       for(i = 0; i < max; i++){
          nums.push(i);
       }
-      // console.log(nums);
 
-      var num1 = document.getElementById("num1").value
-      var num2 = document.getElementById("num2").value
+      var num1 = $("#num1").val();
+      var num2 = $("#num2").val();
 
       var multiples = []
+      // Create a new array with all multiples of num1 and num2 found in max range
       for(i = 0; i < nums.length; i++){
          if(nums[i] % num1 == 0 || nums[i] % num2 === 0 ){
             multiples.push(nums[i]);
             // Need to remove 0
          }
       }
-      // console.log(multiples);
 
       var sum = 0
+      // Sum all the multiples of max range
       for(i = 0; i < multiples.length; i++){
          sum += multiples[i];
       }
 
+      // Fill the span HTML element with the answer of sum
       $("span").text(sum);
    });
 });
