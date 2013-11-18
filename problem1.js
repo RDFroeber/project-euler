b492042bde321275264282ca0f243c31cba845a6
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-18 09:58:21 -0500

    Added user input through HTML

--- problem1.js
+++ problem1.js
@@ -1,22 +1,33 @@
 $(function(){
-   var nums = []
-   for(i = 0; i < 1000; i++){
-      nums.push(i);
-   }
-   // console.log(nums);
+   var button = $("button");
 
-   var multiples = []
-   for(i = 0; i < nums.length; i++){
-      if(nums[i] % 3 == 0 || nums[i] % 5 === 0 ){
-         multiples.push(nums[i]);
-         // Need to remove 0
+   button.click(function(e){
+      e.preventDefault();
+      var max = document.getElementById("max").value
+
+      var nums = []
+      for(i = 0; i < max; i++){
+         nums.push(i);
       }
-   }
-   // console.log(multiples);
+      // console.log(nums);
 
-   var sum = 0
-   for(i = 0; i < multiples.length; i++){
-      sum += multiples[i];
-   }
-   console.log(sum);
+      var num1 = document.getElementById("num1").value
+      var num2 = document.getElementById("num2").value
+
+      var multiples = []
+      for(i = 0; i < nums.length; i++){
+         if(nums[i] % num1 == 0 || nums[i] % num2 === 0 ){
+            multiples.push(nums[i]);
+            // Need to remove 0
+         }
+      }
+      // console.log(multiples);
+
+      var sum = 0
+      for(i = 0; i < multiples.length; i++){
+         sum += multiples[i];
+      }
+
+      $("span").text(sum);
+   });
 });
